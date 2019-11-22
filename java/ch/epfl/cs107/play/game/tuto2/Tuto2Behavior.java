3fc0b41ce6a91948730b07d06114b8b6daf1fc32
Kelvin Kappeler <kelvin.kappeler@gmail.com>
2019-11-22 13:57:50 +0100

    remove Import

--- java/ch/epfl/cs107/play/game/tuto2/Tuto2Behavior.java
+++ java/ch/epfl/cs107/play/game/tuto2/Tuto2Behavior.java
@@ -1,7 +1,6 @@
 package ch.epfl.cs107.play.game.tuto2;
 
 import ch.epfl.cs107.play.game.areagame.AreaBehavior;
-import ch.epfl.cs107.play.game.areagame.Cell;
 import ch.epfl.cs107.play.game.areagame.actor.Interactable;
 import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
