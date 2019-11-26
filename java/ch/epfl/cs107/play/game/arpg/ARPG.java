1774048afa3958c76a579b27fbfaaa132ed003e1
Kelvin Kappeler <kelvin.kappeler@gmail.com>
2019-11-26 14:57:48 +0100

    Ajout début projet

---
+++ java/ch/epfl/cs107/play/game/arpg/ARPG.java
@@ -0,0 +1,7 @@
+package ch.epfl.cs107.play.game.arpg;
+
+import ch.epfl.cs107.play.game.rpg.RPG;
+
+public class ARPG extends RPG {
+
+}
---
+++ java/ch/epfl/cs107/play/game/arpg/actor/ARPGPlayer.java
@@ -0,0 +1,8 @@
+package ch.epfl.cs107.play.game.arpg.actor;
+
+public class ARPGPlayer {
+    @Override
+    public String getTitle() {
+        return "ZeldIC";
+    }
+}
