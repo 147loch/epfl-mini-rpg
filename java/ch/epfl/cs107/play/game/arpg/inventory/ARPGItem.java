ee475a636ea8255c7413a505da6409081633931a
Loïc Herman <contact@147lo.ch>
2019-12-05 09:41:53 +0100

    add positionnable as self paremt for getter

--- java/ch/epfl/cs107/play/game/arpg/inventory/ARPGItem.java
+++ java/ch/epfl/cs107/play/game/arpg/inventory/ARPGItem.java
@@ -1,10 +1,8 @@
 package ch.epfl.cs107.play.game.arpg.inventory;
 
-import ch.epfl.cs107.play.game.areagame.actor.Sprite;
 import ch.epfl.cs107.play.game.inventory.InventoryItem;
 import ch.epfl.cs107.play.game.rpg.actor.RPGSprite;
 import ch.epfl.cs107.play.math.Positionable;
-import ch.epfl.cs107.play.math.Transform;
 
 public enum ARPGItem implements InventoryItem {
 	ARROW("zelda/arrow.icon") {
@@ -20,31 +18,13 @@ public enum ARPGItem implements InventoryItem {
 		}
 	};
 
-	ARPGItem(String iconId) {
+	private String iconId;
 
+	ARPGItem(String iconId) {
+		this.iconId = iconId;
+	}
+
+	public RPGSprite getSprite(Positionable self) {
+		return new RPGSprite(this.iconId, 1.f, 1.f, self);
 	}
 }
-
-
-/*public class ARPGItem extends InventoryItem implements Positionable {
-
-	public final static ARPGItem Arrow = new ARPGItem("Arrow", 0.f, 1, "zelda/arrow.icon");
-	
-	private Sprite sprite;
-	
-	public ARPGItem(String name, float weight, int price, String spriteString) {
-		super(name, weight, price);
-		
-		sprite = new Sprite(spriteString, 1.f, 1.f, this);
-	}
-
-	@Override
-	public Transform getTransform() {
-		return null;
-	}
-
-	@Override
-	public Vector getVelocity() {
-		return null;
-	}
-}*/
